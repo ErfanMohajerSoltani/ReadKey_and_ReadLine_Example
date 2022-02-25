89e0e2cf19695b2fc32c8d77a3a806db20d6f06e
Erfan Mohajer Soltani <erfanm512@gmail.com>
2022-02-26 03:06:55 +0330

    Difference between ReadLine and ReadKey command (with comment description)

--- ReadKey_and_ReadLine_Example/Program.cs
+++ ReadKey_and_ReadLine_Example/Program.cs
@@ -6,7 +6,18 @@ namespace ReadKey_and_ReadLine_Example
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to start!");
+
+            // When we use ReadKey command, console waits for pressing any key to continue
+            Console.ReadKey();
+
+            // When we use ReadLine command, in truth we're giving input and console waits to user enter a value
+            Console.Write("\n" + "Please enter your full name: ");
+
+            string fullName;
+            fullName = Console.ReadLine();
+
+            Console.WriteLine("Hi " + fullName + ", Hope you enjoyed <3" + "\n" + "-----------------------------------------------");
         }
     }
 }
