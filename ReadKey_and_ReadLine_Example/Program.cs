104ec91ce744623c75615e79d20d5cdcb1fcf6d7
Ed <Ed@DESKTOP-LL8D0FB>
2022-02-26 02:33:51 +0330

    Add project files.

---
+++ ReadKey_and_ReadLine_Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReadKey_and_ReadLine_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
